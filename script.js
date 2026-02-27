900829e754c681eea1326156234b4106e674d9f5
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-02-27 11:07:07 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const form = document.getElementById('contact-form')
+const firstName = document.getElementById('first-name')
+const secondName = document.getElementById('last-name')
+const email = document.getElementById('email')
+const querry = document.getElementById('querry')
+
+function {
+
+
+
+}
